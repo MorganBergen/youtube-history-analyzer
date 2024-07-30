95b4e3551a6cd76dec879697743ca26380278afb
morgan bergen <morganmahabergen@icloud.com>
2024-07-30 14:28:17 -0700

    rendering template, uploading file, and returning json

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))
+    if file:
+        data = json.load(file)
+        # process the data here
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
